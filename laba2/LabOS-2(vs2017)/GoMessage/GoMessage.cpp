a855c0a9581b198174c4dc226cecd387bbe2f4d6
Eugene Azanov <azanov46@gmail.com>
2019-04-10 15:45:26 +0500

    Abb 2 laba

---
+++ laba2/LabOS-2(vs2017)/GoMessage/GoMessage.cpp
@@ -0,0 +1,29 @@
+﻿// GoMessage.cpp : Определяет экспортированные функции для приложения DLL.
+//
+
+#include "GoMessage.h"
+#include "resource.h"
+
+WCHAR szStr[50];
+BOOL CALLBACK DlgProc(HWND hwndDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message) {
+	case WM_COMMAND:
+		switch (wParam) {
+		case IDOK:
+			GetDlgItemText(hwndDlg, IDC_EDIT1, szStr, 50);
+			EndDialog(hwndDlg, IDOK);
+			return true;
+		}
+	case IDCANCEL:
+		EndDialog(hwndDlg, IDCANEL);
+		return true;
+	}
+
+	return false;
+}
+
+void inMessage(HMODULE hLib, HWND hWnd, LPWSTR szStr1){
+	DialogBox(hLib, MAKEINTRES(IDD_DIALOG2), hWnd, DlgProc);
+	wcscpy_s(szZtr, szStr1);
+	}
---
+++ laba2/LabOS-2(vs2017)/LabOS-2/Resource.h
@@ -0,0 +1,40 @@
+﻿//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется LabOS-2.rc
+//
+
+#define IDM_MENU_LOAD                   101
+#define IDS_APP_TITLE                   103
+#define IDM_ABOUT                       104
+#define IDS_MESSAGE_BOX                 106
+#define IDI_LABOS2                      107
+#define IDI_SMALL                       108
+#define IDC_LABOS2                      109
+#define IDS_ABOUT                       110
+#define IDR_MAINFRAME                   128
+#define IDR_MENU1                       130
+#define IDD_DIALOG1                     133
+#define IDD_DIALOG2                     134
+#define IDOK                            1000
+#define IDCANCEL                        1001
+#define IDC_EDIT1                       1002
+#define IDM_FILE_EXIT                   32775
+#define IDM_FILE_LOADDLL                32777
+#define IDM_HELP_ABOUT                  32778
+#define ID_32779                        32779
+#define ID_32780                        32780
+#define ID_32781                        32781
+#define ID_32782                        32782
+#define IDM_SHOWDIALOG                  32783
+#define IDC_STATIC                      -1
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32779
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
